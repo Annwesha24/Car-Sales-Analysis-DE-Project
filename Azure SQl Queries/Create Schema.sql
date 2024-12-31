02415cccc70480d35d561b8c81b9c2e6e10e7544
Annwesha24 <annwesha.mondal99@gmail.com>
2024-12-31 11:41:33 +0530

    add: Azure SQL Queries

---
+++ Azure SQl Queries/Create Schema.sql
@@ -0,0 +1,13 @@
+CREATE TABLE source_car_data(
+	Branch_ID varchar(200),
+	Dealer_ID varchar(200),
+	Model_ID varchar(200),
+	Revenue BIGINT,
+	Units_Sold BIGINT,
+	Date_ID varchar(200),
+	Day INT,
+	Month INT,
+	Year INT,
+	BranchName varchar(2000),
+	DealerName varchar(2000)
+)
