1202947cccee45661c9c4b8e051d3911a1bd5519
Annwesha24 <annwesha.mondal99@gmail.com>
2025-01-05 22:23:57 +0530

    add: Validate output -- Databricks

---
+++ Databricks Notebooks/Validate Output.sql
@@ -0,0 +1,7 @@
+SELECT count(*) FROM cars_catalog.gold.factsales
+
+SELECT count(*) FROM cars_catalog.gold.dim_dealer
+
+SELECT count(*) FROM cars_catalog.gold.dim_model
+
+SELECT * FROM cars_catalog.gold.dim_model
